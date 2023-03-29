2ff0289fb46232a9e71bb22aa3f417982458625c
Fidha Rafeeque <fidhahadi@gmail.com>
2023-03-29 12:04:35 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -13,21 +13,25 @@ import Gallery from './Components/Gallery';
 import Footer from './Components/Footer';
 import Tailwindtest from './Components/Tailwindtest';
 import Purchase from './Components/Purchase';
+import logo from './Components/images/logo-green.png'
 
 
 function App() {
   return (
    <>
    <Navbar />
-        <nav className="nav">
-          <Link to="/" className="nav-item">HOME</Link>
+   <div class="na">
+        <nav class="nav">
+        <img class="absolute left-10 top-19 h-15 w-35" src={logo} alt="" />
+          <Link to="/" class="nav-item">HOME</Link>
           <Link to="/about-us" className="nav-item">ABOUT US</Link>
           <Link to="/courses" className="nav-item">COURSES</Link>
           <Link to="/pages" className="nav-item">PAGES</Link>
           <Link to="/blog" className="nav-item">BLOG</Link>
           <Link to="/contact" className="nav-item">CONTACT</Link>
-          <Link to="/search" className="nav-item"><i class="fa fa-search" aria-hidden="true"></i></Link>
+          <Link to="/search" class="nav-item text-green"><i class="fa fa-search" aria-hidden="true"></i></Link>
         </nav>
+        </div>
     <Routes>
     <Route path="/" element={<Introduction />} />
     <Route path="/about-us" element={<Aboutus />} />
